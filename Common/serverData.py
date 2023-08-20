2994069221e3eb9e64bb75ce143ad7addca43e30
clr-cera <93736542+clr-cera@users.noreply.github.com>
2023-08-20 19:43:08 -0300

    Now dns is being done in the right way

--- Common/serverData.py
+++ Common/serverData.py
@@ -1,2 +1,12 @@
-HOST = "icmchat.ddns.net"
+import dns.resolver
+
+def getIPV6(domain: str) -> str:
+    return dns.resolver.resolve(domain, 'AAAA')[0].to_text()
+
+DOMAIN = 'icmchat.ddns.net'
+HOST = getIPV6(DOMAIN)
 PORT = 9998
+
+
+if __name__ == '__main__':
+    print(HOST)
