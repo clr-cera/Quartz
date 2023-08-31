6f3614b1ae6d930041b4cc85f7185e78008c26b0
clr-cera <93736542+clr-cera@users.noreply.github.com>
2023-08-31 07:46:37 -0300

    now it is on flakes!

--- flake.nix
+++ flake.nix
@@ -1,1 +1,29 @@
-a
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils}:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+
+          my-python-packages = ps: with ps; [ 
+          dnspython
+          ];
+        in
+
+        with pkgs;
+        {
+          devShells.default = mkShell {
+            buildInputs = [
+            (pkgs.python3.withPackages my-python-packages)
+            ];
+          };
+        }
+      );
+}
