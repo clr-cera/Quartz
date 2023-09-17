338797f733a0a1d969750a58711b99a878558a19
clr-cera <93736542+clr-cera@users.noreply.github.com>
2023-09-17 17:14:59 -0300

    Trying

--- src/Quartz/ClientLib/clientClass.py
+++ src/Quartz/ClientLib/clientClass.py
@@ -1,25 +1,18 @@
 from threading import Thread
 from socket import socket
 
-import ClientLib
 from ClientLib import clientWire
 from Common import serverData, colors
 from types import ModuleType
+
 import importlib
 import pkgutil
-
-
-def iter_namespace(ns_pkg):
-    # Specifying the second argument (prefix) to iter_modules makes the
-    # returned name an absolute name instead of a relative one. This allows
-    # import_module to work without having to do additional modification to
-    # the name.
-    return pkgutil.iter_modules(ns_pkg.__path__, ns_pkg.__name__ + ".")
 
 discovered_plugins = [
     importlib.import_module(name)
     for finder, name, ispkg
-    in iter_namespace(ClientLib.ClientPlugins)
+    in pkgutil.iter_modules()
+    if name.startswith('client_')
 ]
 
 class Client:
