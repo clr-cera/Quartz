4b2a3dcc3275c66963511d65c90e455f63812c99
clr-cera <93736542+clr-cera@users.noreply.github.com>
2023-09-23 15:33:37 -0300

    Update cowsay.py

--- plugin-example/cowsay.py
+++ plugin-example/cowsay.py
@@ -2,9 +2,9 @@
 This Plugin is just an example of a command plugin, as this is a client plugin, it should be placed at ClientLib/ClientPlugins
 '''
 import os
-from Common.messageLib import Msg
+from Common.messageLib import Msg # type: ignore
 
-def commands(client, message: str, possibleCommand: str, role: str, msgObject: Msg):
+def commands(client, message: str, possibleCommand: str, role: str, msgObject: Msg) ->bool:
     if possibleCommand =="/cowsay":
         message = str(message.split(' ', 1)[1])
         print(role)
@@ -15,3 +15,4 @@ def commands(client, message: str, possibleCommand: str, role: str, msgObject: M
             return True
         else:
             return False
+    return False
