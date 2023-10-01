cc387e00132e7148e04a8b6835227869bd717d59
clr-cera <93736542+clr-cera@users.noreply.github.com>
2023-10-01 13:01:56 -0300

    formatted!

--- flake.nix
+++ flake.nix
@@ -4,66 +4,68 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils}:
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
     flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          inherit (nixpkgs.lib) getExe;
+    (
+      system: let
+        inherit (nixpkgs.lib) getExe;
 
-          pkgs = import nixpkgs {
-            inherit system;
-          };
+        pkgs = import nixpkgs {
+          inherit system;
+        };
 
-          my-python-packages = ps: with ps; [ 
-          dnspython
-          build
-          setuptools
+        my-python-packages = ps:
+          with ps; [
+            dnspython
+            build
+            setuptools
           ];
-          
-        in rec
-        {
-          packages = rec {
-            default = Quartz;
+      in rec
+      {
+        packages = rec {
+          default = Quartz;
 
-            Quartz = pkgs.python3Packages.buildPythonApplication {
-              format = "pyproject";
-              name = "Quartz";
-              pname = "Quartz";
-              src = ./.;
-              propagatedBuildInputs = [ (pkgs.python3.withPackages my-python-packages) ];
+          Quartz = pkgs.python3Packages.buildPythonApplication {
+            format = "pyproject";
+            name = "Quartz";
+            pname = "Quartz";
+            src = ./.;
+            propagatedBuildInputs = [(pkgs.python3.withPackages my-python-packages)];
+          };
+
+          Server = pkgs.writeShellScriptBin "Quartz-Server" ''
+            python ${Quartz}/lib/python3.10/site-packages/Quartz/server.py
+          '';
+
+          Docker = pkgs.dockerTools.buildImage {
+            name = "quartz-docker";
+            tag = "latest";
+            copyToRoot = pkgs.buildEnv {
+              name = "image-root";
+              paths = [pkgs.coreutils pkgs.bash Quartz Server (pkgs.python3.withPackages my-python-packages)];
+              pathsToLink = ["/bin"];
             };
 
-            Server =  pkgs.writeShellScriptBin "Quartz-Server" ''
-              python ${Quartz}/lib/python3.10/site-packages/Quartz/server.py
-            '';
-            
-            Docker = pkgs.dockerTools.buildImage {
-              name = "quartz-docker";
-              tag = "latest";
-              copyToRoot = pkgs.buildEnv {
-                name = "image-root";
-                paths = [ pkgs.coreutils pkgs.bash Quartz Server (pkgs.python3.withPackages my-python-packages) ];
-                pathsToLink = [ "/bin" ];
-              };
-              
-              config = {
-                Cmd = ["/bin/Quartz-Server"];
-              };
+            config = {
+              Cmd = ["/bin/Quartz-Server"];
             };
           };
-          apps = {
-            default = {
-              type = "app";
-              program ="${packages.Quartz}/bin/Quartz";
-            };
-            
-            Server = {
-              type = "app";
-              program =getExe packages.Server;
-            };
-
+        };
+        apps = {
+          default = {
+            type = "app";
+            program = "${packages.Quartz}/bin/Quartz";
           };
-          
-        }
-      );
+
+          Server = {
+            type = "app";
+            program = getExe packages.Server;
+          };
+        };
+      }
+    );
 }
--- shell.nix
+++ shell.nix
@@ -1,14 +1,13 @@
-{ pkgs ? import <nixpkgs> {} }:
-let
-  pythonEnv = pkgs.python3.withPackages(ps: with ps;[ dnspython build ]);
-
+{pkgs ? import <nixpkgs> {}}: let
+  pythonEnv = pkgs.python3.withPackages (ps: with ps; [dnspython build]);
 in
-(pkgs.buildFHSUserEnv {
-  name = "pip-Install-Shell";
-  targetPkgs = pkgs: ([
-    pythonEnv
-    pkgs.python3Packages.pip
-    pkgs.python3Packages.virtualenv
-  ]);
-  runScript = "bash";
-}).env
+  (pkgs.buildFHSUserEnv {
+    name = "pip-Install-Shell";
+    targetPkgs = pkgs: [
+      pythonEnv
+      pkgs.python3Packages.pip
+      pkgs.python3Packages.virtualenv
+    ];
+    runScript = "bash";
+  })
+  .env
